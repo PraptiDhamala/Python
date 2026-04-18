146c7fc93260897c090f4906ed2fb80f8328ce84
praptidhamala <praptidhamala@Praptis-MacBook-Air.local>
2026-04-18 15:25:57 +0545

    Python Basic Day 2

---
+++ first.py
@@ -0,0 +1,25 @@
+#Day Two Python
+# typecasting--->convert from one datatype to another 
+a="1"
+b="6"
+print(int(a)+int(b));
+
+# implicit
+c=9.1
+d=1
+print(c+d)
+#  Taking user input
+a=input("Prapti")
+print(a);
+# string
+x='He said"no"'
+print(x)
+x='He said"no"'
+print(x[0])
+
+for character in x:
+    print(character)
+
+    names="Harryiayaa"
+    print(names[0:-5])
+        # print(len(names))
