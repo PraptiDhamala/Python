af055732a6bbfb0cf3aaf2299c5bce21cf26fc66
praptidhamala <praptidhamala@Praptis-MacBook-Air.local>
2026-04-19 20:29:58 +0545

    Explored about string method

---
+++ string-1.py
@@ -0,0 +1,19 @@
+
+# String Method
+p="Prapti!!!! D!!!Papuu Papuu"
+print(len(p))
+print(p.upper())
+print(p.lower())
+print(p.rstrip("!"))
+print(p.replace("P","S"))
+print(p.split(" "))
+papu="introDuction to fah"
+print(papu.capitalize())
+print(len(papu))
+print(len(papu.center(30)))
+print(p.count("Papuu"))
+print(papu.endswith("fah"))
+print(papu.endswith("fah",8,10))
+str="He's a fine guy. He is an engineer"
+print(str.find("is"))
+# print(str.index("fah"))
