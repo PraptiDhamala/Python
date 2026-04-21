bd1e60c7474cbaee61b489d3f3590a2f263c2ceb
Prapti Dhamala <dhamalaprapti09@gmail.com>
2026-04-21 20:26:07 +0545

    Match case done

---
+++ matchcase.py
@@ -0,0 +1,13 @@
+# new version from 3.1 onward
+x=int(input("enter the value of x: "))
+match x:
+    case 0:
+            print("Zero is the chosen one")
+    case 1:
+            print("Zero is the chosen one")
+    case _ if x!=90:
+            print(x," not 90 lol")
+    case _ if x!=80:
+            print(x," not 80 lol")
+    case _ :
+            print(x)
