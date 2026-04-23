a59c50a1a380182387bfd81c474f58845598ea15
Prapti Dhamala <dhamalaprapti09@gmail.com>
2026-04-23 11:35:20 +0545

    Fucntion in Python done

---
+++ function.py
@@ -0,0 +1,15 @@
+def comparenum(a,b):
+    if(a>b):
+        print(a, " is greater than ", b)
+    else:
+        print(b, " is greater than ", a)
+
+
+def calcugmean(c,d):
+    mean=(c*d)/(c+d)
+    print(mean)
+
+a=20
+b=30
+comparenum(a,b)
+calcugmean(a,b)
