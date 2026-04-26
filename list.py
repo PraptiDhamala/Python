35291fe02cb128bb32f1142f779b1875019af97e
Prapti Dhamala <dhamalaprapti09@gmail.com>
2026-04-26 19:33:51 +0545

    List explaination

--- list.py
+++ list.py
@@ -4,3 +4,13 @@ l=[1,3,9,"Prapti",7,8,10,100,1099,"hi","jiisjs",76,4,2,9]
 # print(l[0])
 # print(l[1])
 # print(l[2])
+# # ordered collection of data items, seperated by comma enclosed in big bracket
+
+
+# # this is also valis, in same list diff data type but that doesn't give this essence
+# l=[1,3,9]
+# print(l)
+# print(type(l))
+# print(l[0])
+# print(l[1])
+# print(l[2])
