eafd2aaeb1063d66fea54891fcd189361a2aafc8
Prapti Dhamala <dhamalaprapti09@gmail.com>
2026-04-26 19:34:48 +0545

    comment added

--- list.py
+++ list.py
@@ -14,3 +14,21 @@ l=[1,3,9,"Prapti",7,8,10,100,1099,"hi","jiisjs",76,4,2,9]
 # print(l[0])
 # print(l[1])
 # print(l[2])
+
+
+
+# # index can be used to access in list
+# print(l[-3])
+# #length of l-3
+# if "3" in l:
+#     print("yes")
+# else:
+#     print("no")
+
+# if "pti" in "Prapti":
+#     print("yes")
+
+# print(l[:])
+# print(l[1:])
+# print(l[-1:])
+# print(l[2:3])
