e6b78647d7747582695c2405db15d7c739543195
Prapti Dhamala <dhamalaprapti09@gmail.com>
2026-04-28 21:10:28 +0545

    Tuple beginning

--- tuple.py
+++ tuple.py
@@ -1,15 +1,2 @@
 tup=(1,5,6)
 print(type(tup))
-print(tup)
-# for single tuple
-x=(1,)
-# python3 tuple.py
-# ordered collection of data items
-# x[0]=90cannot be changed 
-print(x)
-print(tup[0])
-print(tup[1])
-print(tup[2])
-print(tup[-1])
-if 6 in tup:
-    print("present in tuple")
