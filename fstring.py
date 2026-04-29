3eb069ae42934afb9e37d31570866a7646d8afc2
Prapti Dhamala <dhamalaprapti09@gmail.com>
2026-04-29 20:59:34 +0545

    Fstringgg

--- fstring.py
+++ fstring.py
@@ -3,3 +3,8 @@ country="Nepal"
 name="Prapti"
 print(letter.format(name,country))
 # python3 fstring.py
+letter="Hey my name is {1} and I am from {0}"
+country="Nepal"
+name="Prapti"
+print(letter.format(name,country))
+# python3 fstring.py
