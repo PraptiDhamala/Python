c3c5c4a99307adbcf999a1c957b5f34320e0cb70
Prapti Dhamala <dhamalaprapti09@gmail.com>
2026-04-29 21:21:27 +0545

    Fstring done

--- fstring.py
+++ fstring.py
@@ -7,4 +7,9 @@ letter="Hey my name is {1} and I am from {0}"
 country="Nepal"
 name="Prapti"
 print(letter.format(name,country))
-# python3 fstring.py
+# fstring
+print(f"Hey my name is {name} and I am from {country}")
+
+price=99.00011111
+txt=f"The price of the product is {price: .2f}"
+print(txt)
