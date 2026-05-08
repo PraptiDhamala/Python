599ff04723a70cf8a90e1e2d2320f25967b6db9c
Prapti Dhamala <dhamalaprapti09@gmail.com>
2026-05-08 14:12:43 +0545

    Done with dictionary methods

--- dictionary.py
+++ dictionary.py
@@ -1,7 +1,19 @@
 dic={'name':"Prapti", 'age':19, 'address':'Suncity' }
-print(dic)
-print(dic.keys())
-print(dic.values())
+# print(dic)
+# print(dic.keys())
+# print(dic.values())
 
-for key in dic.keys():
-    print(dic[key])
+# for key in dic.keys():
+#     print(dic[key])
+
+# dic={'name':"Prapti", 'age':19, 'address':'Suncity' }
+# dic2={'surname':"Dhamala"}
+# dic.update(dic2)
+# print(dic,dic2)
+# dic.clear()
+dic.popitem()
+print(dic)
+dic.pop('age')
+print(dic)
+del dic['name']
+print(dic)
