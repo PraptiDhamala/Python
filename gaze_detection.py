b279017fafdc85ea6b6ba4f1f70a11721a4f1be1
Prapti Dhamala <dhamalaprapti09@gmail.com>
2026-05-17 17:22:18 +0545

    Done with pupil detection

--- gaze_detection.py
+++ gaze_detection.py
@@ -9,14 +9,22 @@ while True:
 #    roi = frame[50:2000, 200:1300] #from top to bottom, left to right, these number
 
     roi = frame[100:1200, 50:1300] #from top to bottom, left to right, these number 
+    rows, cols, _ = roi.shape
     gray_roi = cv2.cvtColor(roi,cv2.COLOR_BGR2GRAY) #gray pardincha video
     gray_roi = cv2.GaussianBlur(gray_roi, (7,7), 0)
     _, threshold = cv2.threshold(gray_roi, 60, 255, cv2.THRESH_BINARY_INV)
     # video sakiyo or read garna sakena vane break huncha
     contours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours= sorted(contours, key=lambda x: cv2.contourArea(x), reverse=True)
     print(contours)
     for cnt in contours:
-        cv2.drawContours(roi, [cnt], -1, (0,0,255), 3)
+        (x,y,w,h) = cv2.boundingRect(cnt)
+        #  cv2.drawContours(roi, [cnt], -1, (0,0,255), 3)
+        cv2.rectangle(roi, (x,y) ,(x+w,y+h), (255,0,0), 2)
+        cv2.line(roi,(x+int(w/2),0),(x+int(w/2),rows),(0,255,0),2)
+        cv2.line(roi,(0, y+int(h/2)),(cols,y+int(h/2)),(0,255,0),2)
+
+        break
     if not ret:
         print("Video ended or cannot be read.")
         break
