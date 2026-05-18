1f8bcf7522397d640b30024f670996dfbd333022
Prapti Dhamala <dhamalaprapti09@gmail.com>
2026-05-18 22:57:31 +0545

    clear concept about import

---
+++ virtual.py
@@ -0,0 +1,14 @@
+# import, how does it works
+# loading code from python to current script
+import math
+print(math.sqrt(64))
+from math import floor
+print(floor(2.34))
+from math import *
+print( 3* pi)
+print(dir(math))# kk function ra variable constnat cha vancha 
+print(type(math.perm))
+
+from yy import func1
+import yy
+func1()
---
+++ yy.py
@@ -0,0 +1,29 @@
+# finally ma vako jailee execute huncha error aaye ni na aaye ni
+# try:
+#     l=[1,2,34,4]
+#     i=int(input("Enter the index : "))
+#     ptint(l[i])
+# except:
+#     print("error has occured: ")
+# finally:
+#     print("Whatever the situation I am always executed")
+
+# print("Whatever the situation I am always executed")
+
+
+def func1():
+    try:
+        l=[1,2,34,4]
+        i=int(input("Enter the index : "))
+        print(l[i])
+        return 1
+    except:
+        print("error has occured: ")
+        return 0
+    finally:
+        print("Whatever the situation I am always executed") #function execute huda ni huncha 
+
+    print("Whatever the situation I am always executed") #yo hunna
+
+x=func1()
+print(x)
