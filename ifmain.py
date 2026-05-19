d18c2cfd19df73a97c304d67e5ee9d3338b65f2d
Prapti Dhamala <dhamalaprapti09@gmail.com>
2026-05-19 20:47:43 +0545

    Done with if name main

---
+++ ifmain.py
@@ -0,0 +1,3 @@
+import papu
+
+papu.welcome()
---
+++ papu.py
@@ -0,0 +1,8 @@
+def welcome():
+    print("Welcome from Papu")
+print(__name__) # for this chai yo main papu ko lagi papu huncha 
+if __name__ == "__main__": #yetai bata run vayo vane yo run if not not khalko
+    welcome() #function call
+
+    # determine whether the script. is being run directly or being imported ad a module
+    
