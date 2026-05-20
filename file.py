8da7baf21d130070471cf811a18822ebf6e39e9d
Prapti Dhamala <dhamalaprapti09@gmail.com>
2026-05-20 19:39:08 +0545

    File handling

--- file.py
+++ file.py
@@ -1,15 +1,15 @@
-# f=open('myfile.txt','r') # r-------->reading, w-------->writing, a----------->append
-# print(f)
-# text=f.read()
-# print(text)
-# f.close()
-# r is the default mode
+f=open('myfile.txt','r') # r-------->reading, w-------->writing, a----------->append
+print(f)
+text=f.read()
+print(text)
+f.close()
+r is the default mode
 
-# f=open('myfile.txt','w') # r-------->reading, w-------->writing, a----------->append
-# print(f)
-# text=f.read()
-# print(text)
-# f.close()
+f=open('myfile.txt','w') # r-------->reading, w-------->writing, a----------->append
+print(f)
+text=f.read()
+print(text)
+f.close()
 
 
 # f=open('myfile.txt','a') # r-------->reading, w-------->writing, a----------->append
@@ -19,9 +19,9 @@
 # f.close()
 
 f=open('myfile.txt','a') # r-------->reading, w-------->writing, a----------->append
-# print(f)
+print(f)
 text=f.write("You will")
-# print(text)
+print(text)
 # f.close()
 
 with open('myfile.txt','a'):
