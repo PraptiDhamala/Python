b247edbacfd8b736685bae81c291b6d958aa23cb
Prapti Dhamala <dhamalaprapti09@gmail.com>
2026-05-20 19:37:16 +0545

    This is about file halding along with handling with with statement

---
+++ file.py
@@ -0,0 +1,31 @@
+# f=open('myfile.txt','r') # r-------->reading, w-------->writing, a----------->append
+# print(f)
+# text=f.read()
+# print(text)
+# f.close()
+# r is the default mode
+
+# f=open('myfile.txt','w') # r-------->reading, w-------->writing, a----------->append
+# print(f)
+# text=f.read()
+# print(text)
+# f.close()
+
+
+# f=open('myfile.txt','a') # r-------->reading, w-------->writing, a----------->append
+# print(f)
+# text=f.append("You will ")
+# print(text)
+# f.close()
+
+f=open('myfile.txt','a') # r-------->reading, w-------->writing, a----------->append
+# print(f)
+text=f.write("You will")
+# print(text)
+# f.close()
+
+with open('myfile.txt','a'):
+    f.write("You must")
+
+
+
