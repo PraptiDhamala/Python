a1c6e1aa97e028eaeb0ec6b9e946fac230ea5d96
Prapti Dhamala <dhamalaprapti09@gmail.com>
2026-05-25 20:56:57 +0545

    About map and filters

---
+++ filter.py
@@ -0,0 +1,23 @@
+def cube(x):
+    return x*x*x
+
+print((cube(2)))
+
+# python3 filter.py
+l=[1,2,4,6,4,3]
+newl=[]
+for item in l:
+    newl.append(cube(item))
+# newl= list(map(cube,l))
+print (newl)    
+
+
+p=[1,9,4,8,4,3,7]
+newl= list(map(cube,p))
+print (newl)    
+
+def filter_function(a):
+    return a>2
+
+newnewl= list(filter(filter_function,l))
+print(newnewl)
