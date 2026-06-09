4f3096b4780f810e604323b2a8347eee455da95b
Prapti Dhamala <dhamalaprapti09@gmail.com>
2026-06-09 20:27:39 +0545

    Streak

--- examp.py
+++ examp.py
@@ -3,7 +3,7 @@ num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 
 # Add numbers
-result = num1 /num2
+result = num1-num2
 
 # Display result
-print("The sum is:", result)
+print("The diference is:", result)
