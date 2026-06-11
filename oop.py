f817c4ff4cdba70e2c4fbb28c34d579bf23bee16
Prapti Dhamala <dhamalaprapti09@gmail.com>
2026-06-11 20:51:50 +0545

    Constructor in the play

--- oop.py
+++ oop.py
@@ -1,4 +1,23 @@
 # # helps us to map entities ro real world entities
 # class is blueprint
-# objects is entity
+# objects is instance 
 
+
+class person:
+    name="Prapti"
+    occupation="Software Developer"
+    networth=1000
+    def info(self):
+        print(f"{self.name} is a {self.occupation}")
+a= person()
+a.name="Pranjal"
+a.occupation="Footballer"
+print(a.name,a.occupation)
+a.info()
+b= person()
+b.name="Prakriti"
+b.occupation="Actress"
+print(b.name,b.occupation)
+b.info()
+
+# the self parameter is reference to current instance of thr class
