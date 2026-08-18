1d3316b8568f66a5e0c6092f14a5dafdf3ce9938
Prapti Dhamala <dhamalaprapti09@gmail.com>
2026-08-19 00:01:12 +0545

    oop

--- oopp.py
+++ oopp.py
@@ -12,7 +12,7 @@ class Student:
 
 student1 = Student("Pranjal", 17,"USA")
 student2 = Student("Prakriti", 21,"AUS")
-student3 = Student("Prapti", 24,"UK")
+student3 = Student("klkn", 24,"UK")
 
 student1.display()
 student2.display()
