725deae0087a88f4d08d1c46de64e0c7b8efaa85
Prapti Dhamala <dhamalaprapti09@gmail.com>
2026-08-30 05:55:20 +0545

    oop

--- str.py
+++ str.py
@@ -1,7 +1,7 @@
 a = float(input("Enter first number: "))
 b = float(input("Enter second number: "))
 
-print("Addition:", a + b)
+print("ADD:", a + b)
 print("Subtraction:", a - b)
 print("Multiplication:", a * b)
 
