e9162c46260b13b90bf873fecc152af5f81996e9
Prapti Dhamala <dhamalaprapti09@gmail.com>
2026-09-02 14:12:22 +0545

    oop

--- str.py
+++ str.py
@@ -3,8 +3,8 @@ b = float(input("Enter second number: "))
 c = float(input("Enter third number: "))
 
 print("ADD:", a + b + c)
-print("Subtraction:", a - b)
-print("Multiplication:", a * b)
+print("Subtraction:", a - b-c)
+print("Multiplication:", a * b*c)
 
 if b != 0:
     print("Division:", a / b)
