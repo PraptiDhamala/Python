e926891499ec65dc9d546c0c7130d8271bf512ce
Prapti Dhamala <dhamalaprapti09@gmail.com>
2026-09-18 06:52:27 +0545

    oop

--- kk.py
+++ kk.py
@@ -1,8 +1,5 @@
-def check_even_orodd(n):
-    if n % 6 == 0:
-        return "even"
-    else:
-        return "odd"
+def main():
+    print("Hello, World!")
 
-num = float(input("Enter a number: "))
-print(check_even_orodd(num))
+if __name__ == "__main__":
+    main()
