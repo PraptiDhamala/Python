d5a977ea3d99b9ec2c3a8edc9e084219220e0102
Prapti Dhamala <dhamalaprapti09@gmail.com>
2026-09-24 21:38:01 +0545

    oop

--- str.py
+++ str.py
@@ -7,6 +7,7 @@ print("ADD:", a + b + c+d)
 print("Subtraction:", a - b - c-d)
 print("Multiplication:", a * b * c*d)
 print("Factorization:", a!c)
+print("exponential:", a^c)
 
 if b != 0:
     print("Division:", a / b / c/d)
